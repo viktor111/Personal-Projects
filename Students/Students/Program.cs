369d67cf36551c2c3a520ced6759ad16810fc751
Viktor Draganov <swifrorlilko@gmail.com>
2019-03-31 19:24:57 +0300

    Add files via upload

---
+++ Students/Students/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Students
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Number students: ");
+
+            var numberofStudents = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            var students = new Student[numberofStudents];
+
+            for (int i = 0; i < numberofStudents; i++)
+            {
+                var studentName = string.Empty;
+                var studentCourse = string.Empty;
+                var studentGrade = 0;
+
+                Console.Write("Name: ");
+                studentName = Console.ReadLine();
+
+                Console.Write("Course: ");
+                studentCourse = Console.ReadLine();
+
+                Console.Write("Grade: ");
+                studentGrade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                var student = new Student(studentName, studentCourse, studentGrade);
+
+                students[i] = student;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("-Students-");
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student.name}, Course: {student.course}, Grade: {student.grade}");
+            }
+        }
+    }
+}
---
+++ Students/Students/StudentsClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Students
+{
+    class Student
+    {
+        public string name;
+
+        public string course;
+
+        public int grade;
+
+        public Student(string aName, string aCourse, int aGrade)
+        {
+            name = aName;
+            course = aCourse;
+            grade = aGrade;
+        }
+    }
+}
